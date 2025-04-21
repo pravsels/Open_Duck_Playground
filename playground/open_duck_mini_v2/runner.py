3bd906127e2508d7f28b105150d2136ba24df4a9
Praveen Selvaraj <praveen.beatle@gmail.com>
2025-04-21 13:14:44 +0100

    hpc updates

--- playground/open_duck_mini_v2/runner.py
+++ playground/open_duck_mini_v2/runner.py
@@ -40,8 +40,7 @@ def main() -> None:
         default="checkpoints",
         help="Where to save the checkpoints",
     )
-    # parser.add_argument("--num_timesteps", type=int, default=300000000)
-    parser.add_argument("--num_timesteps", type=int, default=150000000)
+    parser.add_argument("--num_timesteps", type=int, default=300000000)
     parser.add_argument("--env", type=str, default="joystick", help="env")
     parser.add_argument("--task", type=str, default="flat_terrain", help="Task to run")
     parser.add_argument(
